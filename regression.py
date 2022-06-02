c874dcb6f5dc43dbc16f9737578c6cdb8b80c7f2
s32k <105165487+s32k@users.noreply.github.com>
2022-06-02 15:40:27 -0400

    Update regression.py

--- regression.py
+++ regression.py
@@ -4,11 +4,24 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+from sklearn import preprocessing
 
-df = pd.read_csv('Foresight/dataset/Ontario.csv')
+
+df = pd.read_csv('/content/drive/MyDrive/Borealis/Ontario.csv')
+scaler = preprocessing.MinMaxScaler()
 X = df[['Number of Homes', 'Population']]
 y = df['Number of Beds']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3)
+print(X)
+print(y)
+
+scaled_X = scaler.fit_transform(X)
+scaled_X = pd.DataFrame(scaled_X, columns = ['Number of Homes', 'Population'])
+print(scaled_X)
+scaled_y = preprocessing.normalize([y])
+scaled_y = pd.DataFrame(scaled_y[0], columns = ['Number of Beds'])
+print(scaled_y)
+X_train, X_test, y_train, y_test = train_test_split(scaled_X, scaled_y, test_size = 1/3)
+
 
 lr = LinearRegression()
 lr.fit(X_train, y_train)
@@ -21,6 +34,7 @@ print(r2)
 y_pred = lr.predict(X_train)
 r2 = r2_score(y_train, y_pred)
 print(r2)
+
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -43,6 +57,7 @@ x2 = X_test['Population'].values
 z2 = y_test.values
 y2 = X_test['Number of Homes'].values
 
+'''
 coefs = lr.coef_
 intercept = lr.intercept_
 print(coefs, intercept)
@@ -50,6 +65,7 @@ xs = np.tile(np.arange(61), (61,1))
 ys = np.tile(np.arange(61), (61,1)).T
 zs = xs*coefs[0]+zs*coefs[1]+intercept
 print("Equation: y = {:.2f} + {:.2f}x1 + {:.2f}x2".format(intercept, coefs[0], coefs[1]))
+'''
 
 ax.scatter(x1, y1, z1, c='r', marker='x')
 ax.scatter(x2, y2, z2, c = 'b', marker = 'x')
