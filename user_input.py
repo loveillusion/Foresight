2d2e9808a2a4facd3abd325a9d2573586381f1c7
Vineel <vineel.nagisetty@borealisai.com>
2022-06-26 16:03:10 -0700

    Refactored file by grouping tasks using functions.
    Added arg parse to take input

--- user_input.py
+++ user_input.py
@@ -1,41 +1,61 @@
 import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
+import argparse
 import csv
 import joblib
 
-province = input("Enter Alpha Code of Province: ")
-provinces = ['Header', 'AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'ON', 'PE', 'QC', 'SK']
-columns = ['prov', 'min_NoH', 'max_NoH', 'min_Pop', 'max_Pop', 'min_NoB', 'max_NoB', 'min_Disc', 'max_Disc']
 
-# Get Required Row:
-maxmin = open('Foresight/datasets/maxmin.csv')
-maxmin = csv.reader(maxmin)
-rows = []
-for row in maxmin:
+def main(province, NoH, Pop):
+    provinces = ['Header', 'AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'ON', 'PE', 'QC', 'SK']
+    columns = ['prov', 'min_NoH', 'max_NoH', 'min_Pop', 'max_Pop', 'min_NoB', 'max_NoB', 'min_Disc', 'max_Disc']
+
+    # Get Required Row:
+    maxmin = open('Foresight/datasets/maxmin.csv')
+    maxmin = csv.reader(maxmin)
+    rows = []
+    for row in maxmin:
         rows.append(row)
-maxmin = rows[provinces.index(province)]
+    maxmin = rows[provinces.index(province)]
 
-# Loading Models:
-m1 = joblib.load('Foresight/models/' + province + '/m1.sav')
-m2 = joblib.load('Foresight/models/' + province + '/m2.sav')
+    # Loading Models:
+    m1 = joblib.load('Foresight/models/' + province + '/m1.sav')
+    m2 = joblib.load('Foresight/models/' + province + '/m2.sav')
 
-# Getting Inputs:
-NoH = int(input("Enter of Homes: "))
-Pop = int(input("Enter Population: "))
+    # Normalization:
+    NoH = (NoH - int(maxmin[columns.index('min_NoH')])) / (
+                int(maxmin[columns.index('max_NoH')]) - int(maxmin[columns.index('min_NoH')]))
+    Pop = (Pop - int(maxmin[columns.index('min_Pop')])) / (
+                int(maxmin[columns.index('max_Pop')]) - int(maxmin[columns.index('min_Pop')]))
 
-# Normalization:
-NoH = (NoH - int(maxmin[columns.index('min_NoH')])) / (int(maxmin[columns.index('max_NoH')]) - int(maxmin[columns.index('min_NoH')]))
-Pop = (Pop - int(maxmin[columns.index('min_Pop')])) / (int(maxmin[columns.index('max_Pop')]) - int(maxmin[columns.index('min_Pop')]))
+    # Run Prediction:
+    dataframe1 = pd.DataFrame([[NoH, Pop]], columns=['Number of Homes', 'Population'])
+    NoB = (m1.predict(dataframe1))[0][0]
+    dataframe2 = pd.DataFrame([[NoH, Pop, NoB]], columns=['Number of Homes', 'Population', 'Number of Beds'])
+    Disc = (m2.predict(dataframe2))[0][0]
 
-# Run Prediction:
-dataframe1 = pd.DataFrame([[NoH, Pop]], columns = ['Number of Homes', 'Population'])
-NoB = (m1.predict(dataframe1))[0][0]
-dataframe2 = pd.DataFrame([[NoH, Pop, NoB]], columns = ['Number of Homes', 'Population', 'Number of Beds'])
-Disc = (m2.predict(dataframe2))[0][0]
+    # Find Actual Prediction:
+    NoB = NoB * (int(maxmin[columns.index('max_NoB')]) - int(maxmin[columns.index('min_NoB')])) + int(
+        maxmin[columns.index('min_NoB')])
+    Disc = Disc * (int(maxmin[columns.index('max_Disc')]) - int(maxmin[columns.index('min_Disc')])) + int(
+        maxmin[columns.index('min_Disc')])
+    prediction = int(NoB + Disc)
+    print(prediction)
 
-# Find Actual Prediction:
-NoB = NoB * (int(maxmin[columns.index('max_NoB')]) - int(maxmin[columns.index('min_NoB')])) + int(maxmin[columns.index('min_NoB')])
-Disc = Disc * (int(maxmin[columns.index('max_Disc')]) - int(maxmin[columns.index('min_Disc')])) + int(maxmin[columns.index('min_Disc')])
-prediction = int(NoB + Disc)
-print(prediction)
+
+def parse_input():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "--province", required=True, help="Alpha Code of Province", type=str, choices=["AB", "BC", "MB", "NB", "ON"]
+    )
+    arg_parser.add_argument(
+        "--NoH", required=False, help="Number of Homes", type=int
+    )
+    arg_parser.add_argument(
+        "--Pop", required=False, help="Population", type=int
+    )
+    return arg_parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_input()
+    main(args.province, args.NoH, args.Pop)
+
