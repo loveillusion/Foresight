f964f3beef7139d2446ab8090d996d02b2f3848e
s32k <105165487+s32k@users.noreply.github.com>
2022-06-26 15:46:02 -0400

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import csv
+import joblib
+
+province = input("Enter Alpha Code of Province: ")
+provinces = ['Header', 'AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'ON', 'PE', 'QC', 'SK']
+columns = ['prov', 'min_NoH', 'max_NoH', 'min_Pop', 'max_Pop', 'min_NoB', 'max_NoB', 'min_Disc', 'max_Disc']
+
+# Get Required Row:
+maxmin = open('Foresight/datasets/maxmin.csv')
+maxmin = csv.reader(maxmin)
+rows = []
+for row in maxmin:
+        rows.append(row)
+maxmin = rows[provinces.index(province)]
+
+# Loading Models:
+m1 = joblib.load('Foresight/models/' + province + '/m1.sav')
+m2 = joblib.load('Foresight/models/' + province + '/m2.sav')
+
+# Getting Inputs:
+NoH = int(input("Enter of Homes: "))
+Pop = int(input("Enter Population: "))
+
+# Normalization:
+NoH = (NoH - int(maxmin[columns.index('min_NoH')])) / (int(maxmin[columns.index('max_NoH')]) - int(maxmin[columns.index('min_NoH')]))
+Pop = (Pop - int(maxmin[columns.index('min_Pop')])) / (int(maxmin[columns.index('max_Pop')]) - int(maxmin[columns.index('min_Pop')]))
+
+# Run Prediction:
+dataframe1 = pd.DataFrame([[NoH, Pop]], columns = ['Number of Homes', 'Population'])
+NoB = (m1.predict(dataframe1))[0][0]
+dataframe2 = pd.DataFrame([[NoH, Pop, NoB]], columns = ['Number of Homes', 'Population', 'Number of Beds'])
+Disc = (m2.predict(dataframe2))[0][0]
+
+# Find Actual Prediction:
+NoB = NoB * (int(maxmin[columns.index('max_NoB')]) - int(maxmin[columns.index('min_NoB')])) + int(maxmin[columns.index('min_NoB')])
+Disc = Disc * (int(maxmin[columns.index('max_Disc')]) - int(maxmin[columns.index('min_Disc')])) + int(maxmin[columns.index('min_Disc')])
+prediction = int(NoB + Disc)
+print(prediction)
